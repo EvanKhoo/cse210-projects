7713383a975df347b12d30e54696e32e5d9cd346
EvanKhoo <kho21002@byui.edu>
2025-09-19 13:10:20 -0600

    Completeted prep 1 and 2 for week 1 of cse 210

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        string firstName = "Bob";
+        Console.WriteLine("Hello " + firstName);
+        Console.WriteLine($"Hello {firstName}");
+        Console.Write($"Hello {firstName}");
+        Console.Write("This is another line\n");
+
+        Console.Write("What is your favorite color? ");
+        string favoriteColor = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {favoriteColor}");
+
+        Console.Write("What is your age? ");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Your age is {age}");
+
+        int a = 13;
+        int b = 7;
+        int c = a + b;
+
+        Console.WriteLine($"C is {c}");
+        Console.WriteLine($"a/b is {a / b}");
+        Console.WriteLine($"{(double)a / b}");
+        Console.WriteLine($"{a % b}");
+
+        double x = 1.5;
+        float y = 3.5f;
+
+        bool isDone = false;
+
+        if (isDone)
+        {
+            Console.WriteLine("All done.");
+        }
+        else
+        {
+            Console.WriteLine("Not done yet.");
+
+        }
+        Console.WriteLine("What is your first name? ");
+        string firstNameBond = Console.ReadLine();
+        string firstNameCapitalized = char.ToUpper(firstNameBond[0]) + firstNameBond.Substring(1).ToLower();
+        Console.WriteLine("What is your last name?");
+        string lastNameBond = Console.ReadLine();
+        string lastNameCapitalized = char.ToUpper(lastNameBond[0]) + lastNameBond.Substring(1).ToLower();
+        Console.WriteLine($"Your name is {lastNameCapitalized}, {firstNameCapitalized} {lastNameCapitalized}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,66 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Please enter your grade percentage: %");
+        int userGrade = int.Parse(Console.ReadLine());
+        char letter = 'G';
+        char sign = ' ';
+
+        //Grade determination logic
+        if (userGrade >= 90 && userGrade <= 100)
+        {
+            letter = 'A';
+        }
+        else if (userGrade >= 80 && userGrade < 90)
+        {
+            letter = 'B';
+        }
+        else if (userGrade >= 70 && userGrade < 80)
+        {
+            letter = 'C';
+        }
+        else if (userGrade >= 60 && userGrade < 70)
+        {
+            letter = 'D';
+        }
+        else if (userGrade < 60)
+        {
+            letter = 'F';
+        }
+        else
+        {
+            Console.WriteLine("Sorry that is not an acceptable grade percentage.");
+        }
+
+        //Assigning signs to the user grade.
+        if (userGrade % 10 >= 7 && userGrade >= 60 && userGrade <= 90)
+        {
+            sign = '+';
+        }
+        else if (userGrade % 10 <= 3 && userGrade >= 60)
+        {
+            sign = '-';
+        }
+        else
+        {
+            sign = ' ';
+        }
+
+        // Printing grades
+        if (userGrade >= 70 && userGrade <= 100)
+        {
+            Console.WriteLine($"You got a {letter}{sign} grade.");
+            Console.WriteLine("Good job you passed the class.");
+        }
+        else if (userGrade < 70 && userGrade >= 0)
+        {
+            Console.WriteLine($"You got a {letter}{sign} grade.");
+            Console.WriteLine("Better try harder next time you failed!");
+        }
+        else
+        {
+            Console.WriteLine("You must be pretty special to get a grade like that.");
+        }
     }
 }
